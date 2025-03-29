6e7811fd232a03ba602928c009e24dceee66e572
Yeung <caosanyang@gmail.com>
2025-03-30 00:28:03 +0800

    Upgrade versionCode & versionName.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.sy.wikitok"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
